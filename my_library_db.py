3db912c98fa1f913fc37d0b7746f384f4b3a730a
Ilkka Kiistala <ilkka.kiistala@gmail.com>
2025-03-26 15:35:28 +0200

    temporary file updating

--- my_library_db.py
+++ my_library_db.py
@@ -11,8 +11,8 @@ column_dtype = {
 }
 
 column_names = list(column_dtype.keys())
-
 new_item = pd.DataFrame()
+update_file = False
 
 # check command line argument was given
 if len(sys.argv) < 2:
@@ -25,12 +25,17 @@ if len(sys.argv) > 2:
   new_item = pd.DataFrame([given_item], columns=column_names, dtype='string')
   print("New book:\n", new_item)
 
+# command line file updating (temporary)
+if len(sys.argv) > 3:
+  if sys.argv[3] == 'update_file':
+    update_file = True
+
 # read text file
 library_file = sys.argv[1]
 
 try:
   # books = pd.read_csv(library_file, sep = '/', header=None, names=['title', 'author', 'isbn', 'year'])
-  books = pd.read_csv(library_file, sep = '/', header=None, names=column_names, dtype=column_dtype)
+  books = pd.read_csv(library_file, sep='/', header=None, names=column_names, dtype=column_dtype)
 except FileNotFoundError:
   print("No such file:", library_file)
   sys.exit(2)
@@ -38,6 +43,9 @@ except FileNotFoundError:
 if len(new_item) > 0:
   books = pd.concat([books, new_item], ignore_index=True)
   books = books.sort_values(by='year')
+
+if update_file:
+  books.to_csv(library_file, sep='/', index=False, header=None)
 
 # handle special case: empty file
 if len(books) == 0:
