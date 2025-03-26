a0cf88199d2020f4f71d4fa5f0f341a884886457
Ilkka Kiistala <ilkka.kiistala@gmail.com>
2025-03-26 15:09:06 +0200

    temporary add using command line

--- my_library_db.py
+++ my_library_db.py
@@ -3,19 +3,41 @@
 import sys
 import pandas as pd
 
+column_dtype = {
+  'title': 'string',
+  'author': 'string',
+  'isbn': 'string',
+  'year': 'string'
+}
+
+column_names = list(column_dtype.keys())
+
+new_item = pd.DataFrame()
+
 # check command line argument was given
 if len(sys.argv) < 2:
   print("Missing argument: filename")
   sys.exit(1)
 
+# command line book adding (temporary)
+if len(sys.argv) > 2:
+  given_item = sys.argv[2].split('/')
+  new_item = pd.DataFrame([given_item], columns=column_names, dtype='string')
+  print("New book:\n", new_item)
+
 # read text file
 library_file = sys.argv[1]
 
 try:
-  books = pd.read_csv(library_file, sep = '/', header=None, names=['title', 'author', 'isbn', 'year'])
+  # books = pd.read_csv(library_file, sep = '/', header=None, names=['title', 'author', 'isbn', 'year'])
+  books = pd.read_csv(library_file, sep = '/', header=None, names=column_names, dtype=column_dtype)
 except FileNotFoundError:
   print("No such file:", library_file)
   sys.exit(2)
+
+if len(new_item) > 0:
+  books = pd.concat([books, new_item], ignore_index=True)
+  books = books.sort_values(by='year')
 
 # handle special case: empty file
 if len(books) == 0:
